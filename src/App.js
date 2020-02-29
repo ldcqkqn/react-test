bc106c897e2ce4fe4a620b3ecc43ff7bb137ddd3
ldcqkqn <40484355+ldcqkqn@users.noreply.github.com>
2020-02-29 17:40:07 +0900

    TOC, Content 컴포넌트 추가 컴포넌트는 정리한다고 생각하자

--- src/App.js
+++ src/App.js
@@ -12,11 +12,38 @@ class Subject extends Component {
   }
 }
 
+class TOC extends Component {
+  render(){
+    return(
+      <nav>
+        <ul>
+          <li><a href = 'https://www.naver.com'>Naver</a></li>
+          <li><a href = 'https://www.adventurer.co.kr'>승부사 온라인</a></li>
+          <li><a href = 'https://www.hanmail.net'>Daum</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>
+        <h2>리액트세계에 온걸 환영한다.</h2>
+        React is funny!!!!
+      </article>
+    );
+  }
+}
+
 class App extends Component {
   render(){
     return (
       <div className="App">
         <Subject></Subject>
+        <TOC></TOC>
+        <Content></Content>
       </div>
     );
   }
