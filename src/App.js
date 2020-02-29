fbddafa207a8dbda8ad21dc069d707e80f4a31b1
ldcqkqn <40484355+ldcqkqn@users.noreply.github.com>
2020-02-29 17:30:22 +0900

    서브젝트 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class Subject extends Component {
+  render(){
+    return(
+      <header>
+        <h1>DongChan</h1>
+        컴포넌트 만들기 1 
+      </header>
+    );
+  }
+}
+
 class App extends Component {
   render(){
     return (
       <div className="App">
-        재밌게 시작해보자
+        <Subject></Subject>
       </div>
     );
   }
